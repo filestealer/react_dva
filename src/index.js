b35ff321b57ba1d4fd9402f6f7aa3635f8dc0706
verb to be <ruslan27032000@gmail.com>
2021-03-17 19:19:20 +0600

    start doing dva

--- src/index.js
+++ src/index.js
@@ -1,20 +1,59 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import {Provider} from "react-redux";
-import {BrowserRouter} from "react-router-dom";
-import store from "./redux/store";
-
-ReactDOM.render(
-    <React.StrictMode>
-        <Provider store={store}>
-            <BrowserRouter>
-                <App/>
-            </BrowserRouter>
-        </Provider>
-    </React.StrictMode>,
-    document.getElementById('root')
-);
+import dva, {connect} from 'dva';
+import axios from "axios";
 
 
+const app = dva();
+
+
+app.model({
+    namespace: 'model',
+    state: {
+        status: '',
+        data: []
+    },
+    effects: {
+        * globalAsync(action, {takeEvery}) {
+            yield takeEvery("REQUEST_POSTS", this.getAsync)
+        },
+        * getAsync(action, {call, put}) {
+            yield put({type: "pending"})
+            try {
+                const payload = yield call(() => axios.get(`https://jsonplaceholder.typicode.com/posts?_limit=5`).then(res => res.data))
+                yield put({type: "success", payload})
+            } catch (e) {
+                yield put({type: "error"})
+            }
+        },
+    },
+    reducers: {
+        pending(state) {
+            return {...state, status: 'pending'}
+        },
+        success(state, {payload: data}) {
+            return {...state, status: 'success', data}
+        },
+        error(state) {
+            return {...state, status: 'error'}
+        },
+    },
+});
+
+
+const App = connect((state) => ({
+    state
+}))(function (props) {
+    console.log(props)
+    return (
+        <div>
+            <h2>Test</h2>
+            <button onClick={() => props.dispatch({type: "REQUEST_POSTS"})}>Test</button>
+        </div>
+    );
+});
+
+
+app.router(() => <App/>);
+
+
+app.start('#root');
