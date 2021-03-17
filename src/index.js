0d9c585d27d488957d45b36e6ac7eccf707db28b
Ruslan <Ruslan27032000@gmail.com>
2021-03-17 22:19:39 +0600

    remove dumbs and add logic for button loading

--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
 import React from 'react';
 import dva, {connect} from 'dva';
 import axios from "axios";
+import {Button, DatePicker} from 'antd';
+import 'antd/dist/antd.css';
+import './index.css'
+
+
+const PENDING = "PENDING"
+const ERROR = "ERROR"
+const SUCCESS = "SUCCESS"
 
 
 const app = dva();
@@ -9,33 +17,45 @@ const app = dva();
 app.model({
     namespace: 'model',
     state: {
-        status: '',
-        data: []
+        statusPosts: '',
+        posts: [],
+        users: [],
+        statusUsers: ''
     },
     effects: {
-        * globalAsync(action, {takeEvery}) {
-            yield takeEvery("REQUEST_POSTS", this.getAsync)
-        },
         * getAsync(action, {call, put}) {
-            yield put({type: "pending"})
+            yield put({type: "pendingPosts"})
+            yield put({type: "pendingUsers"})
             try {
-                const payload = yield call(() => axios.get(`https://jsonplaceholder.typicode.com/posts?_limit=5`).then(res => res.data))
-                yield put({type: "success", payload})
+                const payload_posts = yield call(() => axios.get(`https://jsonplaceholder.typicode.com/posts?_limit=5`).then(res => res.data))
+                const payload_users = yield call(() => axios.get(`https://jsonplaceholder.typicode.com/users?_limit=3`).then(res => res.data))
+                yield put({type: "successPosts", payload: payload_posts})
+                yield put({type: "successUsers", payload: payload_users})
             } catch (e) {
-                yield put({type: "error"})
+                yield put({type: "errorPosts"})
+                yield put({type: "errorUsers"})
             }
         },
     },
     reducers: {
-        pending(state) {
-            return {...state, status: 'pending'}
+        pendingPosts(state) {
+            return {...state, statusPosts: PENDING}
         },
-        success(state, {payload: data}) {
-            return {...state, status: 'success', data}
+        successPosts(state, {payload}) {
+            return {...state, statusPosts: SUCCESS, posts: payload}
         },
-        error(state) {
-            return {...state, status: 'error'}
+        errorPosts(state) {
+            return {...state, statusPosts: ERROR}
         },
+        pendingUsers(state) {
+            return {...state, statusUsers: PENDING}
+        },
+        successUsers(state, {payload}) {
+            return {...state, statusUsers: SUCCESS, users: payload}
+        },
+        errorUsers(state) {
+            return {...state, statusUsers: ERROR}
+        }
     },
 });
 
@@ -43,11 +63,18 @@ app.model({
 const App = connect((state) => ({
     state
 }))(function (props) {
-    console.log(props)
+    const {statusPosts, statusUsers, users, posts} = props.state.model
     return (
         <div>
             <h2>Test</h2>
-            <button onClick={() => props.dispatch({type: "REQUEST_POSTS"})}>Test</button>
+            <Button type="primary" onClick={() => props.dispatch({type: "model/getAsync"})}>
+                {
+                    statusPosts === PENDING || statusUsers === PENDING ?
+                        <i className="fa fa-circle-o-notch fa-spin fa-3x fa-fw"/>
+                        :
+                        "PRESS ME"
+                }
+            </Button>
         </div>
     );
 });
